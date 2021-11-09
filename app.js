c60d8fd9e1ba5c89ededf9de5c5416c2dbec6a0d
abdoul-farah <34750357+abdoul-farah@users.noreply.github.com>
2021-11-09 11:32:30 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ gsap.to(".sur", {y: -50, opacity:1, duration: 1.5 ,ease: "expo", delay:1});
 
 gsap.to(".but-acc", { opacity:0.5, duration: 1.5 ,ease: "expo", delay:3});
 
-
+/*___________________________*/
 
 gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".box0", {
     scrollTrigger: {
         trigger: ".box0",
-        start :"bottom 80%" },// start the animation when ".box" enters the viewport (once)
+        start :"bottom 90%" },// start the animation when ".box" enters the viewport (once)
     y: 100,
     ease:"circ",
     opacity:0
@@ -21,7 +21,7 @@ gsap.from(".box0", {
   gsap.from(".box1", {
     scrollTrigger: {
         trigger: ".box1",
-        start :"bottom 80%" },  // start the animation when ".box" enters the viewport (once)
+        start :"bottom 90%" },  // start the animation when ".box" enters the viewport (once)
     y: 100,
     ease:"circ",
     opacity:0
@@ -30,7 +30,7 @@ gsap.from(".box0", {
   gsap.from(".box2", {
     scrollTrigger: {
         trigger: ".box2",
-        start :"bottom 80%" }, // start the animation when ".box" enters the viewport (once)
+        start :"bottom 90%" }, // start the animation when ".box" enters the viewport (once)
     y: 100,
     ease:"circ",
     opacity:0
@@ -39,7 +39,7 @@ gsap.from(".box0", {
   gsap.from(".box3", {
     scrollTrigger: {
         trigger: ".box3",
-        start :"bottom 80%" }, // start the animation when ".box" enters the viewport (once)
+        start :"bottom 90%" }, // start the animation when ".box" enters the viewport (once)
     y: 100,
     ease:"circ",
     opacity:0
@@ -48,8 +48,30 @@ gsap.from(".box0", {
   gsap.from(".box4", {
     scrollTrigger: {
         trigger: ".box4",
-        start :"bottom 80%" }, // start the animation when ".box" enters the viewport (once)
+        start :"bottom 90%" }, // start the animation when ".box" enters the viewport (once)
     y: 100,
     ease:"circ",
     opacity:0
   });
+
+/*________________________*/
+
+  //Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}

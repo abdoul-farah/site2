0ceae361542e1f181a6d01e32dff801bd3389704
abdoul-farah <34750357+abdoul-farah@users.noreply.github.com>
2021-11-09 13:53:47 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 
+
 gsap.to(".bien", {y: -50, opacity:0.7, duration: 1.5 ,ease: "expo"});
 
 gsap.to(".sur", {y: -50, opacity:1, duration: 1.5 ,ease: "expo", delay:1});
 
 gsap.to(".but-acc", { opacity:0.5, duration: 1.5 ,ease: "expo", delay:3});
+
+
+
 
 /*___________________________*/
 

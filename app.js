e0f754bf1256756f700ab6cd92fa03b68fc19ad2
abdoul-farah <34750357+abdoul-farah@users.noreply.github.com>
2021-11-22 03:01:32 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ gsap.to(".sur", {y: -50, opacity:1, duration: 1.5 ,ease: "expo", delay:1});
 gsap.to(".but-acc", { opacity:0.5, duration: 1.5 ,ease: "expo", delay:3});
 
 
+gsap.to(".ddown", {y: 20, opacity:0.7, duration: 0.7 ,ease: "expo", delay:3, repeat:-1});
 
 
 /*___________________________*/
@@ -52,6 +53,25 @@ gsap.from(".box0", {
   gsap.from(".box4", {
     scrollTrigger: {
         trigger: ".box4",
+        start :"bottom 90%" }, // start the animation when ".box" enters the viewport (once)
+    y: 100,
+    ease:"circ",
+    opacity:0
+  });
+  
+  gsap.from(".resume", {
+    scrollTrigger: {
+        trigger: ".resume",
+        start :"bottom 90%" }, // start the animation when ".box" enters the viewport (once)
+    y: 100,
+    ease:"circ",
+    opacity:0
+  });
+
+  
+  gsap.from(".aboutme", {
+    scrollTrigger: {
+        trigger: ".aboutme",
         start :"bottom 90%" }, // start the animation when ".box" enters the viewport (once)
     y: 100,
     ease:"circ",
